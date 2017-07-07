ca1a0b3d67b73dd4a23bc0574e716ca2cd6f2a87
root <oam1809@hotmail.com>
2017-07-07 11:49:07 -0500

    test server connection

--- test/SpikeScript/20-server-connection-test.sh
+++ test/SpikeScript/20-server-connection-test.sh
@@ -1,13 +1,11 @@
 
 #!/bin/bash
 
-#set -e
+set -e
 source SpikeScript.conf
 echo $USERNAME
 echo $PASSWORD
 echo $IPADDRESS
-sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS free -m
-
-
-#set e+
-
+sshpass -p "vagrant" ssh -tt  vagrant@192.168.88.193 echo "connection success"
+exit
+set e+
