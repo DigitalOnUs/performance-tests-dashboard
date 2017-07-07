426710040fe7056184b6645f7c611b1e235a9fed
root <oam1809@hotmail.com>
2017-07-07 11:56:16 -0500

    updated script

--- test/SpikeScript/20-server-connection-test.sh
+++ test/SpikeScript/20-server-connection-test.sh
@@ -6,6 +6,6 @@ source SpikeScript.conf
 echo $USERNAME
 echo $PASSWORD
 echo $IPADDRESS
-sshpass -p "vagrant" ssh -tt  vagrant@192.168.88.193 echo "connection success"
+sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS echo "connection success"
 exit
 set e+
