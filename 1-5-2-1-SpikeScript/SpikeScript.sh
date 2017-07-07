1e4976c5f21167e3c96a10ef339b56032e6823f8
root <oam1809@hotmail.com>
2017-07-07 00:10:33 -0500

    Add SpikeScript.sh

--- 1-5-2-1-SpikeScript/SpikeScript.sh
+++ 1-5-2-1-SpikeScript/SpikeScript.sh
@@ -15,16 +15,16 @@ while [ $SECONDS -lt $end ]; do
 ##################
 #get data from external server 
 ##################
-MEMORY=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@192.168.15.6 free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+MEMORY=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
 
-DISK=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@192.168.15.6 df -h | awk '$NF=="/"{printf "%s\t\t", $5+0}')
+DISK=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS df -h | awk '$NF=="/"{printf "%s\t\t", $5+0}')
 
-CPU=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@192.168.15.6 top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}')
+CPU=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}')
 
 #For the following metrics, sysstat needs to be installed
-DISKREADSEC=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@192.168.15.6 iostat -dx | tail -n 2 | awk '{ print $4; }')
+DISKREADSEC=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS iostat -dx | tail -n 2 | awk '{ print $4; }')
 
-DISKWRITESEC=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@192.168.15.6 iostat -dx | tail -n 2 | awk '{ print $5; }')
+DISKWRITESEC=$(sshpass -p "$PASSWORD" ssh -tt  $USERNAME@$IPADDRESS iostat -dx | tail -n 2 | awk '{ print $5; }')
 
 ##################
 #POST to InfluxDB
